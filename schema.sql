cf840748e3ce4e6d66c57b4101ecd44d917b52bc
Phantom <fay.ashbro@gmail.com>
2020-10-06 21:34:06 -0400

    schema created

--- schema.sql
+++ schema.sql
@@ -3,23 +3,34 @@ CREATE DATABASE employeeTracker_DB;
 USE employeeTracker_DB;
 
 CREATE TABLE   department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
 
 CREATE TABLE role (
-    id INT PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT, 
     title VARCHAR(30),
     salary DECIMAL, 
-    department_id INT 
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT, 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT, 
-    mangager_id INT 
+    manager_id INT, 
+    PRIMARY KEY (id)
 );
 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('John','Doe',1),('Mike','Chan',2),('Ashley','Rodriguez',3),('Kevin','Tupik',4), ('Malia','Brown',6),('Sarah','Lourd',7),('Tom','Allen',8),('Christian','Eckenrode',9);
+
+INSERT INTO role (title,salary,department_id);
+VALUES ('Sales Lead',100000,''),('Salesperson',80000), ('Lead Engineer',150000),('Software Engineer',120000),('Accountant',125000),('Legal Team Lead',250000),('Lawyer',190000), ('Lead Engineer',150000);
+
+INSERT INTO department (name);
+VALUES ('Sales'), ('Engineering'), ('Finance'),("Legal");
